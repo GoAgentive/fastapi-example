93bb4ad57ff4709db705af93baab028f20e30ac8
dev-substantive <dev@substantive.ai>
2023-10-16 16:31:44 -0700

    added logging

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+import logging
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,9 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    logging.info("Hello World")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    logging.info("Hello World")
     return {"item_id": item_id, "q": q}
